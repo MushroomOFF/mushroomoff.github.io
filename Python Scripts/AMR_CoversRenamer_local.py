f328eea70a81f6a52c6691e4ae1cf5f92aff097a
MushroomOFF <146839496+MushroomOFF@users.noreply.github.com>
2024-06-14 08:10:47 +0300

    Create AMR_CoversRenamer_local.py

---
+++ Python Scripts/AMR_CoversRenamer_local.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+originalRootFolder = '/Users/viktorgribov/Downloads/_Фото/_COVERS/_BIG_test'
+rootFolder = input(f'Type me some path (Enter -> {originalRootFolder}): ')
+if rootFolder == '':
+    rootFolder = originalRootFolder
+for checkFile in os.listdir(rootFolder):
+    if checkFile[len(checkFile)-3:] == 'jpg':
+        nBand, nAlbum, nOther = checkFile.split(' - ')
+        nBandLetter = str(nBand[0])
+        nOutput = str(nOther[:4]) + ' ' + str(nAlbum) + '.jpg'
+        nDirectory = os.path.join(rootFolder, nBandLetter, nBand)
+        cFile = os.path.join(rootFolder, checkFile)
+        nFile = os.path.join(nDirectory, nOutput)
+        if not os.path.exists(nDirectory):
+            os.makedirs(nDirectory)
+        shutil.move(cFile, nFile)
+
+print('Done!')
