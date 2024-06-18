2c04341dcba166057271390622652fba89214ea0
MushroomOFF <146839496+MushroomOFF@users.noreply.github.com>
2024-06-18 13:02:51 +0300

    Update AMR_CoversRenamer_local.py

--- Python Scripts/AMR_CoversRenamer_local.py
+++ Python Scripts/AMR_CoversRenamer_local.py
@@ -9,6 +9,10 @@ for checkFile in os.listdir(rootFolder):
     if checkFile[len(checkFile)-3:] == 'jpg':
         nBand, nAlbum, nOther = checkFile.split(' - ')
         nBandLetter = str(nBand[0])
+        if ord(nBandLetter) >= 1025 and ord(nBandLetter) <= 1105:
+            nBandLetter = 'Русское'
+        elif ord(nBandLetter) < 65:
+            nBandLetter = '0'
         nOutput = str(nOther[:4]) + ' ' + str(nAlbum) + '.jpg'
         nDirectory = os.path.join(rootFolder, nBandLetter, nBand)
         cFile = os.path.join(rootFolder, checkFile)
@@ -16,5 +20,6 @@ for checkFile in os.listdir(rootFolder):
         if not os.path.exists(nDirectory):
             os.makedirs(nDirectory)
         shutil.move(cFile, nFile)
+        print(f'FILE: {checkFile}\nGOTO: {nBandLetter}/{nBand}/{nOutput}\n')
 
 print('Done!')
