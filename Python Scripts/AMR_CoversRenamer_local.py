a625d31c65c9ab4270172049d2b07d82c8a3f3e0
MushroomOFF <146839496+MushroomOFF@users.noreply.github.com>
2024-07-04 09:45:48 +0300

    Update AMR_CoversRenamer_local.py

--- Python Scripts/AMR_CoversRenamer_local.py
+++ Python Scripts/AMR_CoversRenamer_local.py
@@ -7,19 +7,29 @@ if rootFolder == '':
     rootFolder = originalRootFolder
 for checkFile in os.listdir(rootFolder):
     if checkFile[len(checkFile)-3:] == 'jpg':
-        nBand, nAlbum, nOther = checkFile.split(' - ')
-        nBandLetter = str(nBand[0])
-        if ord(nBandLetter) >= 1025 and ord(nBandLetter) <= 1105:
-            nBandLetter = 'Русское'
-        elif ord(nBandLetter) < 65:
-            nBandLetter = '0'
-        nOutput = str(nOther[:4]) + ' ' + str(nAlbum) + '.jpg'
-        nDirectory = os.path.join(rootFolder, nBandLetter, nBand)
-        cFile = os.path.join(rootFolder, checkFile)
-        nFile = os.path.join(nDirectory, nOutput)
-        if not os.path.exists(nDirectory):
-            os.makedirs(nDirectory)
-        shutil.move(cFile, nFile)
-        print(f'FILE: {checkFile}\nGOTO: {nBandLetter}/{nBand}/{nOutput}\n')
+        errorMark = 0
+        textBlockCount = checkFile.count(' - ')
+        if textBlockCount == 2:
+            nBand, nAlbum, nOther = checkFile.split(' - ')
+        elif textBlockCount == 3:
+            nBand, nAlbum, nType, nOther = checkFile.split(' - ')
+            nAlbum = nAlbum + ' [' + nType + ']'
+        else:
+            errorMark = 1
+            print(f'ERROR: {checkFile}\n')
+        if errorMark == 0:
+            nBandLetter = str(nBand[0]).upper()
+            if ord(nBandLetter) >= 1025 and ord(nBandLetter) <= 1105:
+                nBandLetter = 'Русское'
+            elif ord(nBandLetter) < 65:
+                nBandLetter = '0'
+            nOutput = str(nOther[:4]) + ' ' + str(nAlbum) + '.jpg'
+            nDirectory = os.path.join(rootFolder, nBandLetter, nBand)
+            cFile = os.path.join(rootFolder, checkFile)
+            nFile = os.path.join(nDirectory, nOutput)
+            if not os.path.exists(nDirectory):
+                os.makedirs(nDirectory)
+            shutil.move(cFile, nFile)
+            print(f'FILE: {checkFile}\nGOTO: {nBandLetter}/{nBand}/{nOutput}\n')
 
 print('Done!')
